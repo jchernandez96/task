fba7e242f69704fdbc1cc1fb6ff1c39637a19dcf
jchernandez96 <jc.hernandez11@ciaf.edu.co>
2025-09-24 19:51:33 -0500

    Code Actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
   const [tarea, setTarea] = useState("");
   const [Responsable, setResponsable] = useState("");
   const [fecha, setFecha] = useState("");
+  const [datos, setDatos] = useState([]);
+
+  let agregarDatos = () => {
+    let dat = { "tarea": tarea, "responsable": Responsable, "fecha": fecha };
+    setDatos([...datos, dat]);
+  }
 
   return (
     <>
@@ -22,6 +28,31 @@ function App() {
           <input type="date" className='form-control' value={fecha} onChange={(e) => setFecha(e.target.value)} placeholder='Fecha a Realizar' />
         </label>
         {<p>{fecha}</p>}
+        <label className='col-md-3'>Agregar
+          <input type="button" value="Agregar" onClick={agregarDatos} className='btn btn-success' />
+        </label>
+      </div>
+      <div className='container'>
+        <table className='table table-hover'>
+          <thead>
+            <tr>
+              <th>Tarea</th>
+              <th>Responsable</th>
+              <th>Fecha</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              datos.map((dato) => (
+                <tr>
+                  <td>{dato.tarea}</td>
+                  <td>{dato.responsable}</td>
+                  <td>{dato.fecha}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </div>
     </>
   )
